892a18f206c75beacc40a8f4e3ee3e6a9c6b3606
Marcin Kuder <marcin.kuder@gmail.com>
2024-11-18 10:05:39 +0100

    rounded icons

--- app/src/main/java/com/example/barbellcalculator/MainActivity.kt
+++ app/src/main/java/com/example/barbellcalculator/MainActivity.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -94,10 +97,12 @@ fun BarbellCalculator(modifier: Modifier = Modifier) {
                     Spacer(modifier = Modifier.width(8.dp))
                     Box(
                         modifier = Modifier
-                            .background(plate.value)
-                            .padding(4.dp)
+                            .padding(2.dp)
                             .width(4.dp)
                             .height(16.dp)
+                            .size(16.dp)
+                            .clip(RoundedCornerShape(4.dp))
+                            .background(plate.value)
                     )
                 }
             }
